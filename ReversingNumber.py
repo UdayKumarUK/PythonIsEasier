d70ea3114c8606faddc9691b157d78c6688ef469
UdayKumarUK <33616243+UdayKumarUK@users.noreply.github.com>
2017-11-20 01:24:22 +0530

    Update ReversingNumber.py

--- ReversingNumber.py
+++ ReversingNumber.py
@@ -2,7 +2,6 @@
 
 # input from user
 number=128
-number=int(number)
 print('user Input : ',number)
 # variable to store the reversed number
 rev_num=0
@@ -13,3 +12,6 @@ while(int(number) > 0):
     number=int(number)/10
 print('Number After reversing : ',rev_num)
 
+#Other way to Returning reverse of a Number. Below code is applicable only for positive numbers as of now
+num_str = str(number)
+print(num_str[::-1])
